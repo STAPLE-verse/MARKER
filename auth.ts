cc2ff8955ca2d1b8a830819a40def2b0e6d89c60
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2026-06-05 11:13:23 +0200

    auth userid type was string, now it corresponds to STAPLEs numeric

--- auth.ts
+++ auth.ts
@@ -29,7 +29,12 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
           user.hashedPassword
         );
 
-        if (passwordsMatch) return user;
+        if (passwordsMatch) {
+          return {
+            ...user,
+            id: user.id.toString()
+          };
+        }
         return null;
       }
     })
